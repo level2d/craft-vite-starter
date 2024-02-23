bc467ccc8eefd4b8dd40763ff36f5620a173507b
Mario Perez <mario.perez415@gmail.com>
2024-02-23 12:54:30 -0500

    Add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./templates/**/*.{html,twig}", "./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
